10f6825beeef8484cd82746770cc629d535b77d3
RebeccaTobin <52668185+RebeccaTobin@users.noreply.github.com>
2022-05-17 12:05:44 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ TopologyID=1
 pA=0.0 #if > 0, will modify the assortativity of the network. https://en.wikipedia.org/wiki/Assortativity
 Tag=0
 StartTime=0.0183
-tau=0.00915 # 5yr
+tau=0.00183 # 1yr
 k_min=0
 k_max=10000  # k_min <= k <= k_max
 knn_min=0
@@ -23,7 +23,7 @@ r=1.0
 make
 
 # loop through parameter values
-for StartTime in 0.0366 0.0549 0.0732 0.0915 0.1098 0.1281 0.1464 0.1647 0.183
+for StartTime in 0.0366 0.0549 0.0732 0.0915 0.1098 0.1281 0.1464 0.1647 0.183 # onset ages: 20,30,40,50,60,70,80,90,100
 do
     ./main $number $R $gammap $gamman $N $nd $alpha $avgdeg $Mortality $Folder $TopologyID $pA $Tag $StartTime $tau $k_min $k_max $knn_min $knn_max $m $r D
     ./main $number $R $gammap $gamman $N $nd $alpha $avgdeg $Mortality $Folder $TopologyID $pA $Tag $StartTime $tau $k_min $k_max $knn_min $knn_max $m $r C
